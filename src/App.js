6f63b1c76a70c215653943ed3b30d606cd36389f
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-11-12 14:55:50 -0600

    technical difficulties

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home";
 import Order from "./Components/Order";
 import orderSchema from "./orderSchema";
 import * as yup from 'yup'
+import axios from "axios";
 
 const initialOrderValues = {
   name: '',
@@ -22,11 +23,25 @@ const App = () => {
   
   const [orderValues, setOrderValues] = useState(initialOrderValues)
   const [errors, setErrors] = useState(initialError)
+  const [record, setRecord] = useState([])
 
   const validate = (name, value) => {
     yup.reach(orderSchema, name).validate(value)
       .then(() => setErrors({...errors, [name]: ''}))
       .catch(err => setErrors({...errors, [name]: err.errors[0]}))
+  }
+
+  const postRecord = (record) => {
+    axios.post('https://reqres.in/api/orders', record)
+    .then(res => {
+     setRecord([res.data], ...record)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+    .finally(() => {
+      setOrderValues(initialOrderValues)
+    })
   }
 
   const inputChange = (name, value) => {
